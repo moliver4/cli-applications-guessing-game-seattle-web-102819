5a53cc2e1d4ea2281bb695202254d8622ec5e275
Tiffany Tang <tifftang22@gmail.com>
2019-10-04 23:07:53 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,5 +2,10 @@ def run_guessing_game
   my_number = rand(1..6)
   puts "Guess a number between 1 and 6"
   input = gets.chomp
-  
+  if input == my_number
+  elsif input == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry, the computer guessed #{my_number}"
+  end
 end
